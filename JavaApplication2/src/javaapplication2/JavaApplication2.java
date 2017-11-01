895f2d6f87902a52fd22f7f5ee73428b7faa55dc
caglacinar <c.cinar@hotmail.com>
2017-11-01 12:36:01 +0200

    change
    sum

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -6,14 +6,12 @@
 package javaapplication2;
 
 public class JavaApplication2 {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
  int a = 1;  
  int b = 1;
- int sun = a+b ;
+ int sum = a+b ;
 
 
-=======
     public static void main(String[] args) {
         int a = 1;  
         int b = 1;
@@ -24,9 +22,6 @@ public class JavaApplication2 {
     System.out.println(res*res);
     }
 
-}
->>>>>>> c99aa922e15d0c80e6871ebff69ab815fe32a21b
-=======
 
  public int Sum(int a,int b){
      int sum=a;
@@ -36,10 +31,6 @@ public class JavaApplication2 {
      }
      return sum;
  }
-
->>>>>>> 343d4925a18daeb5adf742e45a9db53e792e5237
 }
- 
 
->>>>>>> 343d4925a18daeb5adf742e45a9db53e792e5237
 
