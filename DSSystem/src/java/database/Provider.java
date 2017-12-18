d1ec8c4cea3d8dfc7acd0b81986bae1968f07577
Emre Yilmaz <emre.yilmz@isik.edu.tr>
2017-12-18 15:53:23 +0300

    listDrug

--- DSSystem/src/java/database/Provider.java
+++ DSSystem/src/java/database/Provider.java
@@ -3,7 +3,7 @@ package database;
 public interface Provider {
 
     String DRIVER = "com.mysql.jdbc.Driver";
-    String CONNECTION_URL = "jdbc:mysql://localhost:3306/DS/database";
+    String CONNECTION_URL = "jdbc:mysql://localhost:3306/database";
     String USERNAME = "root";
     String PASSWORD = "root";
 
